31ba94ea61d1f570701eef1c36193e05dce731b2
mkanderson71 <mkanderson71@gmail.com>
2022-05-13 22:40:21 -0400

    fixed it friday

--- public/client.js
+++ public/client.js
@@ -1,32 +1,85 @@
-const dataStore = Immutable.fromJS({
-  rovers: ['Curiosity', 'Opportunity', 'Spirit']
+/**
+ * Global immutable datastore
+ * 
+ * I'm not using this for a lot other than APOD because it's
+ * not very functional to store all the API data here
+ * 
+ */
+
+let state = Immutable.fromJS({
+  rovers: ['Curiosity', 'Opportunity', 'Spirit'],
+  apod: ''
 })
 
-// temporary workspace under grid
-const root = document.getElementById('root') //
+// temporary, move me
+const root = document.getElementById('root')
 
-const getAPOD = () => {
-  const today = new Date();
-  const photodate = new Date(store.get('apod').date)
-  console.debug('photodate is ' + photodate)
+/**
+ * Load APOD and initialize display
+ */
+
+window.addEventListener('load', () => {
+  console.debug('init start')
+  if (!apodCurrent()) {
+    console.log('running init')
+    initApod()
+  }
+})
+
+const apodCurrent = () => {
+  const today = new Date()
+  const photodate = new Date(state.get('apod').date)
+  console.debug('stored APOD date is ' + photodate)
   console.debug('today is ' + today)
+  if (photodate.getDate() !== today.getDate()) {
+    return false
+  } else { return true }
 }
 
-const fetchQuery = (path) => {
+const initApod = async () => {
+  const queryData = await fetchQuery('/apod')
+  state = state.setIn(['apod'], queryData.image)
+  console.log('testing state: ' + state.getIn(['apod', 'explanation']))
+  const apodText = '<div class="grid-item"><table><td style="width:50%">' +
+    '<a href="' + state.getIn(['apod', 'hdurl']) + '">' +
+    '<img src="' + state.getIn(['apod', 'url']) + '" ' +
+    'alt="' + state.getIn(['apod', 'title']) + '"></a></td>' +
+    '<td style="width:50%">' + state.getIn(['apod', 'explanation'])
+    '</td></table></div>'
+  document.getElementById('box0').innerHTML = apodText
+/**   document.getElementById('box1').innerHTML = apodText
+  document.getElementById('box2').innerHTML = apodText
+  document.getElementById('box3').innerHTML = apodText
+  document.getElementById('box4').innerHTML = apodText
+  document.getElementById('box5').innerHTML = apodText
+  document.getElementById('box6').innerHTML = apodText
+  document.getElementById('box7').innerHTML = apodText
+  document.getElementById('box8').innerHTML = apodText
+*/
+}
+
+const fetchQuery = async (path) => {
   const urlPrefix = 'http://localhost:3000'
-  fetch(urlPrefix + path)
-    .then(res => res.json())
-    .then(data => {
-      console.log('2')
-      debugger
-      return data.image
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+  const rawData = await fetch(urlPrefix + path)
+  if (!rawData.ok) {
+    showError('Error fetching ' + path)
+  }
+  const jsonData = await rawData.json()
+  return jsonData
 }
 
-console.log('1')
-const apod = fetchQuery('/apod')
-console.log('3')
-debugger
+// let oRes = {}
+//
+// fetch(urlPrefix + path)
+//   .then(qRes => qRes.json())
+//   .then(jRes => {
+//     oRes = jRes
+//   })
+//   .catch((error) => {
+//     console.error('Error:', error)
+//   })
+// console.log('oRes date is ' + oRes.image.date)
+
+function showError (message) {
+  alert(message)
+}
